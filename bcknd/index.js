8b7b637bc5cc562e3bc7ceda0ce3ccd87a6d6a41
Iair1 <166546999+Iair1@users.noreply.github.com>
2025-08-07 12:06:19 -0300

    tutorial manejo de archivos

--- bcknd/index.js
+++ bcknd/index.js
@@ -1,7 +1,19 @@
+//https://geekflare.com/es/handling-files-in-javascript/
+
 import express from 'express';
+import fs from 'fs';
+import { spawn } from 'child_process';
 
 const app = express();
 app.use(express.json());
-import { spawn } from 'child_process';
+
 
 console.log("jauduiudu")
+
+fs.appendFile("./index.js", "\n//ya se usar appendFile \n//YA TERMINE PROYECTO \n//na mentira", (err) =>{
+    if (err) throw err;
+   console.log("¡Completado!");
+});
+//ya se usar appendFile 
+//YA TERMINE PROYECTO 
+//na mentira
