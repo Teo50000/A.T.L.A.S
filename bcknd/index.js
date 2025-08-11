f8ffb233e5484e65249d6c8e36468751a8eed994
Iair1 <166546999+Iair1@users.noreply.github.com>
2025-08-11 12:01:04 -0300

    agragado de archivos de ejemplo

--- bcknd/index.js
+++ bcknd/index.js
@@ -7,13 +7,3 @@ import { spawn } from 'child_process';
 const app = express();
 app.use(express.json());
 
-
-console.log("jauduiudu")
-
-fs.appendFile("./index.js", "\n//ya se usar appendFile \n//YA TERMINE PROYECTO \n//na mentira", (err) =>{
-    if (err) throw err;
-   console.log("¡Completado!");
-});
-//ya se usar appendFile 
-//YA TERMINE PROYECTO 
-//na mentira
