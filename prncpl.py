0667b092794e190f7411a92a3cab69f94a76d7be
Iair1 <166546999+Iair1@users.noreply.github.com>
2025-08-20 10:28:50 -0300

    funciones de modificacion de archivo

--- prncpl.py
+++ prncpl.py
@@ -1,9 +1,30 @@
+import os
+def crea(txtAgregar, archivo):
+    with open(archivo, 'x') as e:
+        e.write(txtAgregar)
 
-def app(cocos):
-    chkkk = ""
-    with open('nose.txt', 'r') as e:
-        chkkk = e.read()
-    with open('nose.txt', 'w') as f:
-        f.write(cocos + "\n" + chkkk)
-x = input("que desea poner al principio")
-app(x)
+def  modificaDondedice(txtAgregar, archivo, ubicacionenArchivo, lineaAntes, lineaDespues):
+    nts = ""
+    dsps = ""
+    if(lineaAntes == True):
+        nts = "\n"
+    if(lineaDespues == True):
+        dsps = "\n"
+    chueco = ""
+    with open(archivo, 'r') as e:
+        chueco = e.read()
+    ubccionmbr = chueco.find(ubicacionenArchivo)
+    before =  chueco[:ubccionmbr + len(ubicacionenArchivo)].strip()
+    after = chueco[ubccionmbr + len(ubicacionenArchivo):].strip()
+    
+    with open(archivo, 'r+') as f:
+        f.write(before + nts + txtAgregar + dsps + after)
+
+def eliminARchivo (archivo):
+    if(os.path.exists(archivo)):
+        os.remove(archivo)
+    else:
+        print("\n jaja no existeeeeee el archivo ese tuyo\n \n ")
+
+x = input("que archivo desea elimiar? ")
+eliminARchivo(x)
