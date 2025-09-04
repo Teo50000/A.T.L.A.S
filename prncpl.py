caf007af0f3830f7298834ee72308be3190eafe8
Iair1 <166546999+Iair1@users.noreply.github.com>
2025-09-04 12:08:37 -0300

    Update prncpl.py

--- prncpl.py
+++ prncpl.py
@@ -22,16 +22,16 @@ def consiguePromt():
 """
 
 #variables para que el back sepa que hacer
-functionToBeDone = "modificaDondeDice"
+functionToBeDone = "sacarLoQueDice"
 srccc = True           #¿el src de la carpeta esta completo o no?
 ntpo = False         #¿el identificador del archivo es el nombre/vinculo o el tipo de archivo?
-identificadorArch1 = [".txt"] #el archivo principal que sera modificado, o la forma de encontrar los archivos
+identificadorArch1 = ["nose.txt"] #el archivo principal que sera modificado, o la forma de encontrar los archivos
 identificadorArch2 = "" #en caso de involucrar un segundo archivo
-identificadorCarp1 = "C:\\Users\\52218824\\Documents\\GitHub\\A.T.L.A.S" #será la carpeta en la que se encuentra en archivo
+identificadorCarp1 = "C:\\" #será la carpeta en la que se encuentra en archivo
 identificadorCarp2 = "" #en caso de involucrar 2 carpetas
-txt1 = "morir" #en caso de involucrar un texto, se usara este, en funciones de agregar, eleminar, o reemplazar, es el texto que viene antes del agregado y/o el que hay que eliminar
+txt1 = " con gloria " #en caso de involucrar un texto, se usara este, en funciones de agregar, eleminar, o reemplazar, es el texto que viene antes del agregado y/o el que hay que eliminar
 txt2 = ", en realidad me cae mal gloria" #en caso de involucrar 2, este tambien
-walk = False #Todos los archivos solo dentro de una carpeta? o dentro de sus subcarpetas tambien?
+walk = True #Todos los archivos solo dentro de una carpeta? o dentro de sus subcarpetas tambien?
 lineaAntes = True
 lineaDespues = True
 
