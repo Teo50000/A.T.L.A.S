933b950f3220b8148f9b9903972439ac52a1ba22
Iair1 <166546999+Iair1@users.noreply.github.com>
2025-09-25 09:06:27 -0300

    .

--- prncpl.py
+++ prncpl.py
@@ -1,4 +1,12 @@
 import os
+import shutil
+
+
+
+#HAY QUE CAMBIAR LA FUNCION MOVER PARA QUE LA NUEVA UBICACION NO SEA IDENTIFICADORCARP1
+
+
+
 """
 # Importamos Flask para crear el servidor web, request para leer el cuerpo de la petición
 # y jsonify para devolver respuestas en formato JSON fácilmente.
@@ -22,13 +30,13 @@ def consiguePromt():
 """
 
 #variables para que el back sepa que hacer
-functionToBeDone = "sacarLoQueDice"
+functionToBeDone = "mover"
 srccc = True           #¿el src de la carpeta esta completo o no?
-ntpo = True        #¿el identificador del archivo es el nombre/vinculo o el tipo de archivo?
-identificadorArch1 = ["nose.txt"] #el archivo principal que sera modificado, o la forma de encontrar los archivos
+ntpo = False        #¿el identificador del archivo es el nombre/vinculo o el tipo de archivo?
+identificadorArch1 = [".txt"] #el archivo principal que sera modificado, o la forma de encontrar los archivos
 identificadorArch2 = "" #en caso de involucrar un segundo archivo
-identificadorCarp1 = "C:\\" #será la carpeta en la que se encuentra en archivo
-identificadorCarp2 = "" #en caso de involucrar 2 carpetas
+identificadorCarp1 = "C:/Users/52218824/Documents/GitHub/A.T.L.A.S/" #será la carpeta en la que se encuentra en archivo
+identificadorCarp2 = 'pryct' #en caso de involucrar 2 carpetas
 txt1 = " con gloria " #en caso de involucrar un texto, se usara este, en funciones de agregar, eleminar, o reemplazar, es el texto que viene antes del agregado y/o el que hay que eliminar
 txt2 = ", en realidad me cae mal gloria" #en caso de involucrar 2, este tambien
 walk = True #Todos los archivos solo dentro de una carpeta? o dentro de sus subcarpetas tambien?
@@ -41,32 +49,34 @@ def interpreta(prompt):
     print("dd")
     archivo_s = []
     if(identificadorCarp1 != "" and identificadorCarp1[0] != "C"):
-        print("ayudaaaaaaaa")
+        print("encontrand carpeta por nombre")
         identificadorCarp1 = encontrarCarPorNombre(identificadorCarp1)
         print("carpeta 1 identificada")
         print(identificadorCarp1)
     else:
         print(identificadorCarp1)
     if(identificadorCarp2 != "" and identificadorCarp2[0] != "C"):
+        print(identificadorCarp2 + "\n")
         identificadorCarp2 = encontrarCarPorNombre(identificadorCarp2)
+        print(identificadorCarp2)
 
     if(ntpo == False):
         if(walk == True):
+            print(f"encontrando archivos tipo {identificadorArch1} en subcarpetas de {identificadorCarp1}")
             archivo_s = walkCarp(identificadorArch1[0], identificadorCarp1)
             print("linea 54")
-            print(archivo_s)
         else:
+            print(f"encontrando archivos tipo {identificadorArch1} en {identificadorCarp1}")
             archivo_s = encontrartipoencarpeta(identificadorArch1[0], identificadorCarp1)
             print("linea 58")
-            print(archivo_s)
     elif(identificadorArch1[0] != "C" and functionToBeDone != "crea"):
+        print(f"buscando archivos de nombre {identificadorArch1}")
         archivo_s = encontrArchPorNombre(identificadorArch1[0])
         print("linea 62")
-        print(archivo_s)
     else:
         archivo_s = identificadorArch1
         print("linea 66")
-        print(archivo_s)
+    print(archivo_s)
     x = len(archivo_s)
     print("identificado")
     if(functionToBeDone == "crea"):
@@ -152,25 +162,17 @@ def eliminARchivo (archivo):
         return("archivo no existe")
 
 def copiar(archivo, nuevaUbicacion):
-    chueco = ""
-    posicioNombre = archivo.rfind("/")
-    nombre = archivo[posicioNombre + 1:]
     try:
-        with open(archivo, 'r') as e:
-            chueco = e.read()
-    except FileNotFoundError:
-        print("\n No existe el archivo indicado")
-        return("no existe")
-    try:
-        with open(nuevaUbicacion + nombre, 'x') as f:
-            f.write(chueco)
+        shutil.copy(archivoM, nuevaUbicacionM)
     except FileExistsError:
-        print("\n La copia que desea generar ya existe")
-        return("copia preexistente")
+        print("Repetido")
 
 def mover(archivoM, nuevaUbicacionM):
-    copiar(archivoM, nuevaUbicacionM)
-    eliminARchivo(archivoM)
+    try:
+        shutil.move(archivoM, nuevaUbicacionM)
+    except shutil.Error as e:
+        print(f"Carpeta repetida")
+    
 
 def encontrartipoencarpeta(terminacion, carpeta):
     try:
@@ -211,9 +213,10 @@ def encontrarCarPorNombre(carpeta):
     for(root, dirs, files) in os.walk('C:\\', topdown=True):
         for i in range(len(dirs)):
             if(dirs[i] == carpeta):
-                return os.path.join(root, dirs[i])
+                return os.path.join(root, dirs[i]) + "\\"
 
 def walkCarp(terminacion, carpeta):
+    carpeta = carpeta.replace("/", "\\")
     rutas: list[str] = []
     for (root,dirs,files) in os.walk(carpeta, topdown=True):
         for i in range(len(files)):
@@ -228,7 +231,7 @@ def walkCarp(terminacion, carpeta):
 
 
 
-interpreta("papafrita")
+interpreta("")
 
 
 """
