d31bd475e66316c36620427238be6e5075724267
Iair1 <166546999+Iair1@users.noreply.github.com>
2025-10-22 15:54:57 -0300

    Update prncpl.py

--- prncpl.py
+++ prncpl.py
@@ -32,15 +32,15 @@ def consiguePromt():
 
 #variables para que el back sepa que hacer
 disco = "C" #por default se trabajará en el disco C, si se desea cambiarlo se puede
-functionToBeDone = "crea"
+functionToBeDone = "sacarLoQueDice"
 srccc = True        #¿el src de la carpeta esta completo o no?
-ntpo = True       #¿el identificador del archivo es el nombre/vinculo o el tipo de archivo?
-identificadorArch1 = ["noo:ssda.txt", "ESTEESTABIEN.txt"] #el archivo principal que sera modificado, o la forma de encontrar los archivos
+ntpo = True      #¿el identificador del archivo es el nombre/vinculo o el tipo de archivo?
+identificadorArch1 = ["antártica_experimento.js"] #el archivo principal que sera modificado, o la forma de encontrar los archivos
 identificadorArch2 = "" #en caso de involucrar un segundo archivo
-identificadorCarp1 = "C:/Users/52218824/Documents/GitHub/A.T.L.A.S/" #será la carpeta en la que se encuentra en archivo
+identificadorCarp1 = "" #será la carpeta en la que se encuentra en archivo
 identificadorCarp2 = "C:/Users/52218824/" #en caso de involucrar 2 carpetas
-txt1 = "aertyu" #en caso de involucrar un texto, se usara este, en funciones de agregar, eleminar, o reemplazar, es el texto que viene antes del agregado y/o el que hay que eliminar
-txt2 = "YZ" #en caso de involucrar 2, este tambien
+txt1 = "—" #en caso de involucrar un texto, se usara este, en funciones de agregar, eleminar, o reemplazar, es el texto que viene antes del agregado y/o el que hay que eliminar
+txt2 = "" #en caso de involucrar 2, este tambien
 walk = False #Todos los archivos solo dentro de una carpeta? o dentro de sus subcarpetas tambien?
 lineaAntes = False
 lineaDespues = False
@@ -48,7 +48,7 @@ lineaDespues = False
 #esta se usa solo en archivoAaPDF y PDFaTexto
 nombres = ["hola", "adios", "REPÚBLICA DEMOCRÁTICA Y POPULAR DE COREA"]   #no usar variable a menos que sea estrictamente necesario
 replicar = True
-terminacion = ".txt"
+terminacion = ".js"
 
 def interpreta(prompt):
     #interpretacion
