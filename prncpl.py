3373252c3f5f0ff77c22f4e6bd36a4dadc3b6af2
Iair1 <166546999+Iair1@users.noreply.github.com>
2025-10-23 12:09:08 -0300

    Update prncpl.py

--- prncpl.py
+++ prncpl.py
@@ -32,11 +32,11 @@ def consiguePromt():
 
 #variables para que el back sepa que hacer
 disco = "C" #por default se trabajará en el disco C, si se desea cambiarlo se puede
-functionToBeDone = "renombrar"
-srccc = False          #¿el src de la carpeta esta completo o no?
-ntpo = True    #¿el identificador del archivo es el nombre/vinculo o el tipo de archivo?
-identificadorArch1 = ["corrupto.txt"] #el archivo principal que sera modificado, o la forma de encontrar los archivos
-identificadorArch2 = "oid.docx" #en caso de involucrar un segundo archivo
+functionToBeDone = "sacarLoQueDice"
+srccc = True          #¿el src de la carpeta esta completo o no?
+ntpo = True        #¿el identificador del archivo es el nombre/vinculo o el tipo de archivo?
+identificadorArch1 = ["C:/Users/52218824/Documents/GitHub/A.T.L.A.S/nose.txt"] #el archivo principal que sera modificado, o la forma de encontrar los archivos
+identificadorArch2 = "corrupto.docx" #en caso de involucrar un segundo archivo
 identificadorCarp1 = "C:/Users/52218824/Documents/GitHub/A.T.L.A.S/" #será la carpeta en la que se encuentra en archivo
 identificadorCarp2 = "C:/Users/52218824/Favorites/" #en caso de involucrar 2 carpetas
 txt1 = ", educacion, patria, y familiasalud" #en caso de involucrar un texto, se usara este, en funciones de agregar, eleminar, o reemplazar, es el texto que viene antes del agregado y/o el que hay que eliminar
